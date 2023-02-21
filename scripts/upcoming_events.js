b59f50b7ca7c739fc8b497ab22434b8118dc7b60
María Rosa González <gomaroluz@gmail.com>
2023-02-21 15:30:05 -0300

    Tarjetas Dinámicas

--- scripts/upcoming_events.js
+++ scripts/upcoming_events.js
@@ -174,28 +174,54 @@ var data = {
 
 current_date = data.currentDate;
 events = data.events
-past_events = []
 future_events = []
 
 for (var evento in events) {
-    if (events[evento].date < current_date) {
-        
-        past_events.unshift(events[evento])
-        console.log('el evento ya paso');
-    }
-    else {
-        console.log('es un evento futuro');
+    if (events[evento].date > current_date) {
         future_events.unshift(events[evento])
-
+        console.log('es un evento futuro');
     }
 }
 
-console.log(past_events)
+
+future_events.sort((date1, date2) => { 
+    if (date1.date < date2.date) {
+        return -1;
+    } else if (date1.date > date2.date) {
+        return 1;
+    }else {
+        return 0;
+    }
+});
+
 console.log(future_events)
 
- 
+let card = document.getElementById('cards_events');
 
+future_events.map((x) => {
+    card.innerHTML += `   
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="${x.image}" class="card-img-top" alt="Party">
+                        <div class="card-body">
+                            <h5 class="card-title">${x.name}</h5>
+                            <h5 class="card-title">${x.date}</h5>
+                            <p class="card-text"></p>
+                            <p class="card-text"><i>${x.description}</i></p>
+                            <p class="card-text"><b>Category: </b>${x.category}</p>
+                            <p class="card-text"><b>Place: </b>${x.place}</p>
+                            <p class="card-text"><b>Capacity: </b>${x.capacity}</p>
+                            <p class="card-text"><b>Assistance: </b>${x.assistance}</p>
+                            <p class="card-text"><b>Price: </b>${x.price}</p>
 
-
-
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="badge bg-success">Price</a>
+                            <a href="#" class="badge bg-warning text-dark">See more</a>
+                        </div>
+                    </div>
+                </div>
+                    `;
+                    
+});
 
